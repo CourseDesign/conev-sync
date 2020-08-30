c8cfcd7672add077ae121d4edcf4a79a7f6252a5
Ara Park <siyual.bak@gmail.com>
2020-08-30 19:56:33 +0900

    Feature/set up core (#1)

---
+++ package/conev-core/src/config.ts
@@ -0,0 +1,48 @@
+import merge, { Options } from 'deepmerge';
+import Sources, { Source } from './sources';
+
+export default class Config {
+  private readonly sources: Sources;
+
+  private readonly options?: Options;
+
+  private readonly envs: string[];
+
+  private values: Record<string, unknown> | null;
+
+  constructor(sources: Source[], envs: string[], options?: Options) {
+    this.sources = new Sources(sources, options);
+    this.envs = envs;
+    this.options = options;
+    this.values = null;
+
+    this.sync();
+  }
+
+  sync(): Config {
+    const source = this.sources.export();
+    const configs = this.envs
+      .map((env) => source.get(env))
+      .filter((value) => value != null) as Record<string, unknown>[];
+
+    this.values = merge.all(configs, this.options) as Record<string, unknown>;
+
+    return this;
+  }
+
+  get(key = ''): unknown | null {
+    const tokens: string[] = key.split('.').reverse();
+
+    let current: any = this.values;
+    for (
+      let token = tokens.pop();
+      token != null && token.length > 0;
+      token = tokens.pop()
+    ) {
+      if (current == null) return null;
+      current = current[token];
+    }
+
+    return current;
+  }
+}
---
+++ package/conev-core/src/sources.ts
@@ -0,0 +1,47 @@
+import merge, { Options } from 'deepmerge';
+
+export interface Source {
+  export(): Map<string, Record<string, unknown>>;
+}
+
+export class Sources implements Source {
+  private readonly sources: Source[];
+
+  private readonly options?: Options;
+
+  constructor(sources: Source[], options?: Options) {
+    this.sources = sources;
+    this.options = options;
+  }
+
+  add(source: Source, priority = -1): Sources {
+    if (priority === -1) this.sources.push(source);
+    else this.sources.splice(priority, 0, source);
+
+    return this;
+  }
+
+  export(): Map<string, Record<string, unknown>> {
+    const configs = this.sources.map((source) => source.export());
+    const mergedConfig = new Map<string, Record<string, unknown>[]>();
+
+    configs.forEach((config) => {
+      config.forEach((value, key) => {
+        if (!mergedConfig.get(key)) mergedConfig.set(key, []);
+        mergedConfig.get(key)?.push(value);
+      });
+    });
+
+    const config = new Map<string, Record<string, unknown>>();
+    mergedConfig.forEach((value, key) => {
+      config.set(
+        key,
+        merge.all(value, this.options) as Record<string, unknown>
+      );
+    });
+
+    return config;
+  }
+}
+
+export default Sources;
