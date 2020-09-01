715461d6d2af6bfbfd096fd069d8bb0bfe43d988
Ara Park <siyual.bak@gmail.com>
2020-09-01 09:16:34 +0900

    Publish 1.1 (#9)

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,1 +1,31 @@
-test.todo('Add test');
+import { ConfigBuilder, JsonSource } from './index';
+
+describe('conev', () => {
+  const source1 = new JsonSource();
+  source1.set('default', { source: '1', port: 3000 });
+  source1.set('develop', { env: 'develop', port: 3001 });
+
+  const source2 = new JsonSource();
+  source2.set('default', {
+    source: '2',
+    port: 3000,
+    db: { port: 7070, username: 'test', type: 'mysql' },
+  });
+  source2.set('production', { env: 'production', port: 3002 });
+
+  test('get', () => {
+    const config = new ConfigBuilder()
+      .addSource(source1, source2)
+      .addEnv('default', 'develop')
+      .build();
+
+    expect(config.get()).toEqual({
+      source: '2',
+      env: 'develop',
+      port: 3001,
+      db: { port: 7070, username: 'test', type: 'mysql' },
+    });
+    expect(config.get('source')).toEqual('2');
+    expect(config.get('db.port')).toEqual(7070);
+  });
+});
