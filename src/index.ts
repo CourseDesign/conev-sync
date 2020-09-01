8b11d2a80ffc34180d7a5d79d61303337ed94beb
Ara Park <siyual.bak@gmail.com>
2020-09-01 09:03:56 +0900

    feature/publish 1.1 (#8)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,2 @@
-export * from 'conev-core';
-export { default as JsonSource } from 'conev-json-source';
+export * from 'conev-sync-core';
+export { default as JsonSource } from 'conev-sync-json-source';
