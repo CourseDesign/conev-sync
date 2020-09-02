32f8b491ac004e982bf94331677b0c37b5c4c473
Ara Park <siyual.bak@gmail.com>
2020-09-02 12:47:14 +0900

    feature/change export (#17)

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-export * from 'conev-sync-core';
+export {
+  default,
+  ConfigBuilder,
+  Config,
+  Source,
+  Sources,
+} from 'conev-sync-core';
 export { default as JsonSource } from 'conev-sync-json-source';
